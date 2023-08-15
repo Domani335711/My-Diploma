25b7bc422e1dce2fe335ad638f04f1b4065ad708
Domani335711 <123840443+Domani335711@users.noreply.github.com>
2023-08-15 16:39:59 +0300

    add all files

---
+++ js/tab2.js
@@ -0,0 +1,37 @@
+// Food (табы)
+window.addEventListener('DOMContentLoaded',()=>{
+
+    const tabs = document.querySelectorAll('.tabheader__item');
+    const tabContent = document.querySelectorAll('tabcontent');
+    const tabParent = document.querySelector('.tabheader');
+
+    function hideTabContent(){
+        tabContent.forEach(item =>{
+            item.style.display="none";
+        });
+        tabs.forEach(item=>{
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i=0){
+        tabContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabParent.addEventListener('click',(event)=>{
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item,i)=>{
+                if(target==item){
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
+// Конец Food (табы)
